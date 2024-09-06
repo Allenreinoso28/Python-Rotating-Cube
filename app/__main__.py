63569c7e4004d47d8d13a4a98988c1496a453652
Allenreinoso28 <Allen.j.reinoso@gmail.com>
2024-09-06 00:55:51 -0400

    Drawing the square

--- app/__main__.py
+++ app/__main__.py
@@ -1,9 +1,11 @@
 import pygame
 import pygame.tests
+import numpy as np
 
 pygame.init()
 
-window_size = (800, 800)
+WINDOW_SIZE = 800
+window_size = (WINDOW_SIZE, WINDOW_SIZE)
 screen = pygame.display.set_mode(window_size)
 
 pygame.display.set_caption('Cube Go Burrrrr')
@@ -12,25 +14,68 @@ clock = pygame.time.Clock()
 
 fps = 60
 
+#projection matrix converts 3d coord into 2d coord
+projection_matrix = np.matrix([[1,0,0],
+                              [0,1,0],
+                              [0,0,0]])
+
 #vertices
+cube_points = [n for n in range(8)]
+cube_points[0]= np.matrix([[1],[1],[1]])
+cube_points[1]= np.matrix([[1],[-1],[1]])
+cube_points[2]= np.matrix([[-1],[1],[1]])
+cube_points[3]= np.matrix([[-1],[-1],[1]])
+cube_points[4]= np.matrix([[1],[1],[-1]])
+cube_points[5]= np.matrix([[1],[-1],[-1]])
+cube_points[6]= np.matrix([[-1],[1],[-1]])
+cube_points[7]= np.matrix([[-1],[-1],[-1]])
 
+## dot product is the function that does what i want
+# result = np.dot(orthogonal_matrix, cube_points[0])
+# print(result)
 
-#colours
-black = (0,0,0)
+#how to access int from matrix where cube_points[point][row,column]
+# print(cube_points[0][0,0])
+
+def multipleMatrices(a,b):
+    result = np.dot(a,b)
+    return result
+
+# colours
+black = (0, 0, 0)
 white = (255, 255, 255)
+red = (255, 0, 0)
+
+#positioning values
+SCALE = 100
+OFFSET = WINDOW_SIZE/2
 
 running = True
 while running:
+    angle_a = angle_b = angle_c = 0
+    clock.tick(fps)
+    screen.fill(black)
+
+    #rotation matrix goes here
+
+
+    for point in cube_points:
+        #apply rotation to point
+
+        #convert point to a 2coord and draw
+        point_2d = multipleMatrices(projection_matrix, point)
+        x = point_2d[0,0] * SCALE + OFFSET
+        y = point_2d[1,0] * SCALE + OFFSET
+        pygame.draw.circle(screen, red, (x,y), 7)
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill(black)
-
-    pygame.display.flip()
-
-    clock.tick(fps)
-
-    pygame.draw.circle()
     
+
+    angle_a += 0.1
+    pygame.display.update()   
+
 pygame.quit()
